4a58891876569feb529b6c53febcfa4863b8698b
noble <birunoble@gmail.com>
2023-12-04 23:42:27 +0300

    debug the first question check 3

--- _printf.c
+++ _printf.c
@@ -1,45 +1,34 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - Custom printf() function
+ *
+ * @format: format
+ *
+ * Return: Printed characters
  */
-int _printf(const char * const format, ...)
+
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
-
+	int count;
 	va_list args;
-	int i = 0, j, len = 0;
 
+	count = 0;
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-Here:
-	while (format[i] != '\0')
+	while (*format != '\0')
 	{
-		j = 13;
-		while (j >= 0)
+		if (*format == '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
+			count += handle_format(*(++format), args);
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else
+		{
+			count += write(1, format, 1);
+		}
+		++format;
 	}
 	va_end(args);
-	return (len);
+	return (count);
 }
